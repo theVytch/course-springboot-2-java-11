54771cb3d15d1958f7f219f592fb9244fc62359c
TheVytch <edu.vytch@gmail.com>
2021-09-10 17:05:08 -0300

    Order, Instant, ISO 8610

--- src/main/java/com/eduardo/couse/config/TestConfig.java
+++ src/main/java/com/eduardo/couse/config/TestConfig.java
@@ -1,5 +1,6 @@
 package com.eduardo.couse.config;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,7 +8,9 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.eduardo.couse.entities.Order;
 import com.eduardo.couse.entities.User;
+import com.eduardo.couse.repositories.OrderRepository;
 import com.eduardo.couse.repositories.UserRepository;
 
 @Configuration
@@ -16,12 +19,19 @@ public class TestConfig implements CommandLineRunner{
 	
 	@Autowired
 	private UserRepository userRepository;
+	@Autowired
+	private OrderRepository orderRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null,"Maria Brown", "Maria@gmail.com","44997707755", "123456");
 		User u2 = new User(null,"Alex Green", "Alex@gmail.com","4498875563", "123457");
-	
+		
+		Order o1 = new Order(null, Instant.parse("2021-06-20T19:53:07Z"), u1); 
+		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), u2); 
+		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"), u1);
+		
 		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 	}
 }
---
+++ src/main/java/com/eduardo/couse/services/OrderService.java
@@ -0,0 +1,26 @@
+package com.eduardo.couse.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.eduardo.couse.entities.Order;
+import com.eduardo.couse.repositories.OrderRepository;
+
+@Service
+public class OrderService {
+
+	@Autowired
+	private OrderRepository orderRepository;
+	
+	public List<Order> findAll(){
+		return orderRepository.findAll();
+	}
+	
+	public Order findById(Long id) {
+		Optional<Order> obj = orderRepository.findById(id);
+		return obj.get();
+	}
+}
