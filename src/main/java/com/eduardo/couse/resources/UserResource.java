b21a11780b14eda69012a2ecd5233b55f5c58b38
TheVytch <edu.vytch@gmail.com>
2021-09-10 14:46:33 -0300

    camada de servico, registro de componente

--- src/main/java/com/eduardo/couse/resources/UserResource.java
+++ src/main/java/com/eduardo/couse/resources/UserResource.java
@@ -1,19 +1,34 @@
 package com.eduardo.couse.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.eduardo.couse.entities.User;
+import com.eduardo.couse.services.UserService;
 
 @RestController
 @RequestMapping("/users")
 public class UserResource {
 	
+	@Autowired
+	private UserService userService;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L,"maria","maria@gmail.com","4497707777","123123");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = userService.findAll();
+		return ResponseEntity.ok().body(list);
 	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = userService.findById(id);
+		return ResponseEntity.ok().body(obj);
+	}
+	
 }
