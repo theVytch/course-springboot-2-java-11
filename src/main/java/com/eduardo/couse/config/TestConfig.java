e9d8be0695f7a8c1119644962f9e5995299661ab
TheVytch <edu.vytch@gmail.com>
2021-09-13 14:44:53 -0300

    OrderStatus enum

--- src/main/java/com/eduardo/couse/config/TestConfig.java
+++ src/main/java/com/eduardo/couse/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.eduardo.couse.entities.Order;
 import com.eduardo.couse.entities.User;
+import com.eduardo.couse.entities.enums.OrderStatus;
 import com.eduardo.couse.repositories.OrderRepository;
 import com.eduardo.couse.repositories.UserRepository;
 
@@ -27,9 +28,9 @@ public class TestConfig implements CommandLineRunner{
 		User u1 = new User(null,"Maria Brown", "Maria@gmail.com","44997707755", "123456");
 		User u2 = new User(null,"Alex Green", "Alex@gmail.com","4498875563", "123457");
 		
-		Order o1 = new Order(null, Instant.parse("2021-06-20T19:53:07Z"), u1); 
-		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), u2); 
-		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"), u1);
+		Order o1 = new Order(null, Instant.parse("2021-06-20T19:53:07Z"), OrderStatus.PAID, u1); 
+		Order o2 = new Order(null, Instant.parse("2021-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMEN, u2); 
+		Order o3 = new Order(null, Instant.parse("2021-07-22T15:21:22Z"), OrderStatus.WAITING_PAYMEN, u1);
 		
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
